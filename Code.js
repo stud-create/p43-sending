7167b00433d324ec02f724086db9b900c5e22d16
stud-create <81812386+stud-create@users.noreply.github.com>
2021-06-26 14:20:44 +0530

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,13 @@
+const accessCode1 = "darkness";
+const accessCode2 = "hole";
+const accessCode3 = "nose";
+
+function clues() {
+    textSize(20)
+    fill("black")
+    text("Hint: The more there ,the less you see !!", 100,70)
+// add code for displaying rest of the names and the hints.
+  text("Hint: The more you take away , the more it gets", 550,180)
+  text("Hint: it runs yet it has no legs ,what is it!!", 300,300)
+
+}
